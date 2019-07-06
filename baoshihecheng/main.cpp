25456096459421c369f9f01617cc5a58cc651271
20181105879 <2605753145@qq.com>
2019-07-06 09:16:45 +0800

    people in group
    people in group

--- baoshihecheng/main.cpp
+++ baoshihecheng/main.cpp
@@ -1,38 +1,41 @@
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <algorithm>
+#include <iostream>
+using namespace std;
 int main()
 {
-    int n;
+    int n,i;
+    double sum=0,sum1=0,sum2=0;
     scanf("%d",&n);
-    for(int i=0;i<n;i++)
+    int s[n];
+    for(i=0;i<n;i++)
     {
-        int s[11000]={0};
-        int m,k;
-        scanf("%d",&m);
-        for(int j=0;j<m;j++)
-        {
-            int a,b;
-            scanf("%d%d",&a,&b);
-            s[a]=s[a]+b;
-        }
-        for(k=1;k<10100;k++)
-        {
-            int t=s[k]/3;
-            s[k]=s[k]%3;
-            s[k+1]=s[k+1]+t;
-        }
-        int sum=0;
-        for(k=1;k<10100;k++)
-        {
-            if(s[k]!=0)
-            {
-                sum+=s[k];
-            }
-        }
-        
-        printf("Case #%d:\n%d\n",i+1,sum);
+        scanf("%d",&s[i]);
+        sum+=s[i];
     }
-    
-    
+    sort(s,s+n);
+    for(i=0;i<n/2;i++)
+    {
+        sum1+=s[i];
+    }
+    sum2=sum1+s[i];
+    if(n%2==0)
+    {
+        printf("Outgoing #: %d\nIntroverted #: %d\nDiff = %.0f",n/2,n/2,fabs(2*sum1-sum));
+    }
+    else
+    {     if(fabs(2*sum1-sum)>fabs(2*sum2-sum))
+    {
+        printf("Outgoing #: %d\nIntroverted #: %d\nDiff = %.0f",n-n/2,n/2,fabs(2*sum1-sum));
+    }
+    else
+    {
+        printf("Outgoing #: %d\nIntroverted #: %d\nDiff = %.0f",n/2+1,n-n/2-1,fabs(2*sum2-sum));
+    }
+        
+    }
     
     return 0;
 }
