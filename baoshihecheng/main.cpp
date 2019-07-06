d9c9a9776f452722749b96a7447005db22aebd73
20181105879 <2605753145@qq.com>
2019-07-06 09:11:11 +0800

    baoshihecheng
    baoshihecheng

--- baoshihecheng/main.cpp
+++ baoshihecheng/main.cpp
@@ -1,15 +1,38 @@
-//
-//  main.cpp
-//  baoshihecheng
-//
-//  Created by s20181105879 on 2019/7/6.
-//  Copyright © 2019 s20181105879. All rights reserved.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        int s[11000]={0};
+        int m,k;
+        scanf("%d",&m);
+        for(int j=0;j<m;j++)
+        {
+            int a,b;
+            scanf("%d%d",&a,&b);
+            s[a]=s[a]+b;
+        }
+        for(k=1;k<10100;k++)
+        {
+            int t=s[k]/3;
+            s[k]=s[k]%3;
+            s[k+1]=s[k+1]+t;
+        }
+        int sum=0;
+        for(k=1;k<10100;k++)
+        {
+            if(s[k]!=0)
+            {
+                sum+=s[k];
+            }
+        }
+        
+        printf("Case #%d:\n%d\n",i+1,sum);
+    }
+    
+    
+    
     return 0;
 }
